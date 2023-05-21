b80d01b1b67c8de419cb6898332389963a43bf8d
Davide Baret <dbaret@ethz.ch>
2023-05-21 14:18:10 +0200

    Subscribe to odom and goal pose

--- create3_state_machine/include/create3_state_machine/Create3StateMachine.hpp
+++ create3_state_machine/include/create3_state_machine/Create3StateMachine.hpp
@@ -2,6 +2,8 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 #include "irobot_create_msgs/action/undock.hpp"
 
 namespace create3_state_machine
@@ -23,6 +25,13 @@ public:
 private:
   using UndockAction = irobot_create_msgs::action::Undock;
   rclcpp_action::Client<UndockAction>::SharedPtr undock_client_ptr_;
+
+  rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr goal_pose_sub_;
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+
+  void goal_pose_callback(const geometry_msgs::msg::PoseStamped & msg) const;
+
+  void odom_callback(const nav_msgs::msg::Odometry & msg) const;
 
   void send_goal_undock();
 };
--- create3_state_machine/src/Create3StateMachine.cpp
+++ create3_state_machine/src/Create3StateMachine.cpp
@@ -5,10 +5,23 @@ namespace create3_state_machine
 Create3StateMachine::Create3StateMachine(const std::string& name) : Node(name)
 {
   this->undock_client_ptr_ = rclcpp_action::create_client<UndockAction>(this, "undock");
+
+  goal_pose_sub_ = this->create_subscription<geometry_msgs::msg::PoseStamped>("/goal_pose", 10, std::bind(&Create3StateMachine::goal_pose_callback, this, std::placeholders::_1));
+  odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>("/odom", 10, std::bind(&Create3StateMachine::odom_callback, this, std::placeholders::_1));
+
   send_goal_undock();
 }
 
 Create3StateMachine::~Create3StateMachine() = default;
+
+void Create3StateMachine::goal_pose_callback(const geometry_msgs::msg::PoseStamped & msg) const{
+
+  RCLCPP_INFO_STREAM(this->get_logger(), msg.pose.position.x);
+}
+
+void Create3StateMachine::odom_callback(const nav_msgs::msg::Odometry & msg) const{
+  RCLCPP_INFO_STREAM(this->get_logger(), msg.pose.pose.position.x);
+}
 
 void Create3StateMachine::send_goal_undock()
 {
